60ddf8e9af26a354b9747f4e7ad9fdd44a6c7abb
jorgeluis4800 <jorgeluisgs573@gmail.com>
2023-11-29 10:49:59 -0600

    Update properties and import file to integrate values into the table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
 insert into alumnos (matricula, nombre, apellido_paterno, apellido_materno) values (182975, 'Prueba', 'García', 'Saldicvar');
+
+insert into materias(materia, calificacion, id_alumno) values ("matematicas", 10, 1);
